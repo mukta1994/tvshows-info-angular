4f7c0f38ef9cb1c08407553a0397f958229996d0
Mukta Patil <muktapatil3658@gmail.com>
2020-09-14 09:24:11 +0200

    added new component season episodes to display season with details

---
+++ angular10tv-shows-info/src/app/season-episodes/season-episodes.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SeasonEpisodesComponent } from './season-episodes.component';
+
+describe('SeasonEpisodesComponent', () => {
+  let component: SeasonEpisodesComponent;
+  let fixture: ComponentFixture<SeasonEpisodesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SeasonEpisodesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SeasonEpisodesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
