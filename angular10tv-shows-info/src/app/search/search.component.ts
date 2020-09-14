d777b355091bcda14ec4525b5d9605d5c6e8b6aa
Mukta Patil <muktapatil3658@gmail.com>
2020-09-14 17:16:29 +0200

    search bar made common to other pages

--- angular10tv-shows-info/src/app/search/search.component.ts
+++ angular10tv-shows-info/src/app/search/search.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
 
 
 @Component({
@@ -8,17 +14,61 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  @Output() searchEvent = new EventEmitter();
+  i = 0;
+  shows = [];
+  test = "";
 
- 
-  constructor(private dataService: DataService) { }
-  searchText="";
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: any;
+
+
+  constructor(private dataService: DataService, private router: Router) { }
+  searchText = "";
 
   ngOnInit(): void {
-  //   this.dataService.sendGetRequest().subscribe((data:{ results: any[]} )=>{
-  //     console.log(data);
-  //     this.dataset = data.results;
-  // });
-}
+    //   this.dataService.sendGetRequest().subscribe((data:{ results: any[]} )=>{
+    //     console.log(data);
+    //     this.dataset = data.results;
+    // });
+    if (this.myControl.value != null) {
+      this.filteredOptions = this.myControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this.getData(value))
+      );
+    }
+  }
+
+  // private _filter(value: string): string[] {
+  //   const filterValue = value.toLowerCase();
+
+  //   return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  // }
+
+  onSubmit(search: string) {
+    this.router.navigate(['results/' + search], {
+    });
+    this.shows = [];
+    this.searchEvent.emit(search);
+  }
+
+  getData(searchText) {
+    if (searchText != "") {
+      this.shows = [];
+      this.dataService.sendGetRequest(searchText, 1).subscribe((data: { results: any[] }) => {
+        this.shows = [...this.shows];
+        data.results.map((item) => {
+          this.dataService.getDetails(item.id).subscribe((data) => {
+            this.shows.push(data)
+          })
+        })
+      });
+    }
+    else
+    this.shows = [];
+
+  }
 
 
 }
